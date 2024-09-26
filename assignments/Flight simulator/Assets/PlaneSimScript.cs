44152b5dbaa673ddf1843c425a572165d0ea8a54
iceage2114 <31936203+iceage2114@users.noreply.github.com>
2024-09-25 21:16:19 -0400

    pc commit

---
+++ assignments/Flight simulator/Assets/PlaneSimScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using TMPro;
+
+public class PlaneScript : MonoBehaviour
+{
+    // Remember to assign a value to this variable in the Unity editor!
+    // I.e. Drag the camera into the slot in the inspector when you select the plane.
+    public GameObject cameraObject;
+
+    public Terrain terrain; 
+
+    //public TMP_Text scoreText;
+
+    public int score;
+
+    // These variables will control how the plane moves
+    float forwardSpeed = 12f;
+    float xRotationSpeed = 90f;
+    float yRotationSpeed = 90f;
+    float zRotationSpeed = 90f; 
+    float boostTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get directional input (up, down, left, right)
+        float hAxis = Input.GetAxis("Horizontal"); // -1 if left is pressed, 1 if right is pressed, 0 if neither
+        float vAxis = Input.GetAxis("Vertical"); // -1 if down is pressed, 1 if up is pressed, 0 if neither
+
+        float pitchInput = Input.GetAxis("Vertical");
+        float yawInput = Input.GetKey(KeyCode.E) ? -1 : Input.GetKey(KeyCode.Q) ? 1 : 0;
+        float rollInput = Input.GetKey(KeyCode.D) ? -1 : Input.GetKey(KeyCode.A) ? 1 : 0;
+        
+        // Apply the rotation based on the inputs
+        Vector3 amountToRotate = new Vector3(0,0,0);
+        amountToRotate.x = vAxis * xRotationSpeed;
+        amountToRotate.y = yawInput * yRotationSpeed;
+        amountToRotate.z = rollInput * zRotationSpeed;
+        amountToRotate *= Time.deltaTime; // amountToRotate = amountToRotate * Time.deltaTime;
+        transform.Rotate(amountToRotate, Space.Self);
+
+        //colliding with terrain 
+        /*
+        float terrainHeight = terrain.SampleHeight(transform.position);
+        if(terrainHeight > transform.position.y) {
+            forwardSpeed = 0; 
+        } */
+
+        // Make the plane move forward by adding the forward vector to the position.
+        transform.position += transform.forward * forwardSpeed * Time.deltaTime;
+
+        // Position the camera
+        Vector3 cameraPosition = transform.position;
+        cameraPosition += -transform.forward * 10f; // Negative forward points in the opposite direction as forward
+        cameraPosition += Vector3.up * 8f; // Vector3.up is (0,1,0)
+        cameraObject.transform.position = cameraPosition;
+        // LookAt is a utility function that rotates a transform so that it looks at a point
+        cameraObject.transform.LookAt(transform.position);
+    }
+
+
+    // Unity will tell the function below to run under the following conditions:
+    //  1. Two objects with colliders are colliding
+    //  2. At least one of the objects' colliders are marked as "Is Trigger"
+    //  3. At least one of the objects has a Rigidbody
+    public void OnTriggerEnter(Collider other)
+    {
+        // 'other' is the name of the collider that just collided with the object
+        // that this script is attached to (the plane).
+        // Check to see that it has the tag "collectable". Tags are assigned in the Unity editor.
+        if (other.CompareTag("collectable"))
+        {
+            score++;
+            //scoreText.text = "Score " + score;
+            Destroy(other.gameObject);
+        }
+    }
+}
